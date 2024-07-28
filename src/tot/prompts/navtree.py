4110926bbe79bba62c3bbeb6c2c28decab956714
PatrickAllenCooper <patrick.allen.cooper@gmail.com>
2024-07-28 15:53:49 -0600

    Corrected bad task prompts

--- src/tot/prompts/navtree.py
+++ src/tot/prompts/navtree.py
@@ -1,10 +1,8 @@
-cot_prompt = ''''''
+cot_prompt = '''{input}'''
 
-# standard_prompt = '''
-# Write a coherent passage of 4 short paragraphs. The end sentence of each paragraph must be: {input}
-# '''
+standard_prompt = '''{input}'''
 
-vote_prompt = '''Given an instruction and several choices, decide which choice is most promising. Analyze each choice in detail, then conclude in the last line "The best choice is {s}", where s the integer id of the choice.
+vote_prompt = '''You are guiding a robot to its target location given a natural language insturction. Given an instruction and several choices, decide which choice is most promising. Analyze each choice in detail, then conclude in the last line "The best choice is {s}", where s the integer id of the choice.
 '''
 
 compare_prompt = '''Briefly analyze the practicality of the following two plans of action. Conclude in the last line "The more practical approach is 1", "The more practical approach is 2", or "The two approaches are similarly practical".
